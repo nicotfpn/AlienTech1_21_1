42ac294e6f116e661840d7e7581c46807189e521
nicotfpn <128562053+nicotfpn@users.noreply.github.com>
2025-07-30 14:11:25 -0300

    changes

--- src/main/java/net/nicotfpn/alientech/item/ModItems.java
+++ src/main/java/net/nicotfpn/alientech/item/ModItems.java
@@ -1,20 +1,20 @@
-package net.nicotfpn.alientech.item;
+    package net.nicotfpn.alientech.item;
 
-import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import net.nicotfpn.alientech.AlienTech;
+    import net.minecraft.world.item.Item;
+    import net.neoforged.bus.api.IEventBus;
+    import net.neoforged.neoforge.registries.DeferredItem;
+    import net.neoforged.neoforge.registries.DeferredRegister;
+    import net.nicotfpn.alientech.AlienTech;
 
-import javax.swing.*;
+    import javax.swing.*;
 
-public class ModItems {
-    public static final DeferredRegister.Items  ITEMS = DeferredRegister.createItems(AlienTech.MODID);
+    public class ModItems {
+        public static final DeferredRegister.Items  ITEMS = DeferredRegister.createItems(AlienTech.MODID);
 
-    public static final DeferredItem<Item> GRAVITON = ITEMS.register("graviton",
-        () -> new Item(new Item.Properties()));
+        public static final DeferredItem<Item> GRAVITON = ITEMS.register("graviton",
+            () -> new Item(new Item.Properties()));
 
-    public static void register(IEventBus eventBus){
-        ITEMS.register(eventBus);
+        public static void register(IEventBus eventBus){
+            ITEMS.register(eventBus);
+        }
     }
-}
